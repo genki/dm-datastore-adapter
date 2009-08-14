bfe470b85a5021cba023a2539191c0df927d8997
Genki Takiuchi <genki@s21g.com>
2009-08-14 20:52:46 +0900

    Fixed spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'rubygems'
+require 'appengine-api-1.0-sdk-1.2.0.jar'
 require 'dm-core'
 require 'dm-aggregates'
 require 'dm-types'
